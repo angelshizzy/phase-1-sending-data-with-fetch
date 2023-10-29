6bb849c2b7f0077cd8ad7b15fb554a085f6c6d0f
Angela Shirao <angela.shirao@student.moringaschool.com>
2023-10-29 20:24:57 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Add your code here
+function submitData(name,email){
+    const apiUrl= "http://localhost:3000";
+
+const data = {
+    name:name,
+    email: email,
+};
+
+return fetch(apiUrl, {
+    method: "POST",
+    headers:{
+        "Content-Type":"application/json",
+        "Accept" : "application/json",
+    },
+    body:JSON.stringify(data),
+})
+
+.then(response=> response.json())
+.then((data)=> {
+    const id = data.id;
+    const idElement= document.createElement("p");
+    idElement.textContent = `New ID:${id}`;
+    document.appendChild(idElement);
+    return data;
+
+})
+
+.catch((error)=> {
+    const errorElement = document.createElement("p");
+    errorElement.textContent= `Error:${error.message}`;
+    document.body.appendChild(idElement);
+    throw error;
+});
+
+}
+
+const userName = "Angela Shirao";
+const userEmail = "angelashirao1@gmail.com";
+
+submitData(userName,userEmail)
+.then(response=> console.log(response))
+.catch(error => console.error(error))
